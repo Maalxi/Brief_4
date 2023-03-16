a2c994ea080593392aa1f7f203631fa9153d1cca
Maalxi <maalxi38@gmail.com>
2023-03-16 16:18:59 +0100

    Mise à jour le 16-03-2023 - update

--- inc/ts/style.ts
+++ inc/ts/style.ts
@@ -1,6 +1,16 @@
 const button: HTMLButtonElement | null = document.querySelector('button');
 const section1: HTMLButtonElement | null = document.querySelector('.page_accueil');
+const section2: HTMLButtonElement | null = document.querySelector('.premiere_page_question');
+const section3: HTMLButtonElement | null = document.querySelector('.premiere_page_reponse');
+const section4: HTMLButtonElement | null = document.querySelector('.page_de_fin');
 
-button.addEventListener('click', () => {
-  section1.style.display = 'none';
-});
+
+if (button !== null && section1 !== null && section3 !== null && section4 !== null) {
+  button.addEventListener('click', () => {
+    section1.style.display = 'none';
+    section3.style.display = 'none';
+    section4.style.display = 'none';
+  });
+}
+
+
