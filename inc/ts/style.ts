ab514e9a7a4483b8fa8871be109c3b9ddb65cd7e
Maalxi <maalxi38@gmail.com>
2023-03-21 10:47:16 +0100

    Mise à jour le 21-03-2023 - update

--- inc/ts/style.ts
+++ inc/ts/style.ts
@@ -1,19 +1,37 @@
 // Bouton start - Lancement du quiz
 
 const button: HTMLButtonElement | null = document.querySelector('button');
-const section1: HTMLButtonElement | null = document.querySelector('.page_accueil');
-const section2: HTMLButtonElement | null = document.querySelector('.premiere_page_question');
-const section3: HTMLButtonElement | null = document.querySelector('.premiere_page_reponse');
-const section4: HTMLButtonElement | null = document.querySelector('.page_de_fin');
+const section1: HTMLElement | null = document.querySelector('.page_accueil');
+const section2: HTMLElement | null = document.querySelector('.premiere_page_question');
+const section3: HTMLElement | null = document.querySelector('.premiere_page_reponse');
+const section4: HTMLElement | null = document.querySelector('.page_de_fin');
+
+
+// Afficher la section1 et cacher les autres sections
+
+if (section1) {
+  section1.style.display = 'block';
+}
+
+if (section2) {
+  section2.style.display = 'none';
+}
+
+if (section3) {
+  section3.style.display = 'none';
+}
+
+if (section4) {
+  section4.style.display = 'none';
+}
+
 
 
 if (button !== null && section1 !== null && section2 !== null && section3 !== null && section4 !== null) {
   button.addEventListener('click', () => {
-    section1.style.display = 'none';
-    section3.style.display = 'none';
-    section4.style.display = 'none';
+    section:nth-child(1) {
+      /* styles pour afficher la setion */
+          section1.style.display = 'block';
+    }
   });
 }
-
-
-// 
