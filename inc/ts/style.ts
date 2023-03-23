a7bb82f1794b1ff75b99601ac02f82383f84d0c0
Maalxi <maalxi38@gmail.com>
2023-03-23 12:33:00 +0100

    Mise à jour le 23-03-2023 - update 12h32

--- inc/ts/style.ts
+++ inc/ts/style.ts
@@ -1,4 +1,4 @@
-import datajson from "../json/question_quizz.json"
+import datajson from "../json/question_quizz.json";
 
 // Toutes les sections
 
@@ -44,13 +44,23 @@ const valeurReponseD: string | null | undefined =
 const button_next: Element | null = document.querySelector("#button_next");
 const button_info: Element | null = document.querySelector("#button_info");
 
+// Boutton next pour la section anecdote
+
+const button_next_anecdote = document.querySelector(".button_next_anecdote");
+
+// Bouton Quitter - Retour au début du quizz
+
+const button_fin: HTMLButtonElement | null = document.querySelector(
+  ".button_fin > button"
+);
+
 // Choix d'une questions
 
 const reponses = [reponse_A, reponse_B, reponse_C, reponse_D];
 
 // Tableau des réponses valides pour chaque question
 
-const reponsesValides = ["B", "C", "A", "D"];
+const reponsesValides = ["D", "B", "A", "B", "B"];
 
 // Validation du pseudo lors de l'appui sur la touche Entrée
 // Validation du pseudo et passage à la section suivante lors du clic sur le bouton Start
@@ -84,6 +94,7 @@ function validateUsername() {
       section1.classList.add("hide");
       section2.classList.remove("hide");
       section2.classList.add("show");
+      button_info?.classList.add("hide")
     }
   } else {
     alert("Le pseudo ne peut pas être vide !");
@@ -92,45 +103,126 @@ function validateUsername() {
 
 // Récupération des questions depuis le JSON
 
-const current_question_index = 0;
+let current_question_index = 0;
 
-const current_question = datajson.Question[current_question_index].question;
+// Afficher la première question dès le début
 
-const A = datajson.Question[current_question_index].propositions[0];
-const B = datajson.Question[current_question_index].propositions[1];
-const C = datajson.Question[current_question_index].propositions[2];
-const D = datajson.Question[current_question_index].propositions[3];
+const current_question: string =
+  datajson.Question[current_question_index].question;
 
-const h3_question = document.querySelector('.h3_question');
+const A: string = datajson.Question[current_question_index].propositions[0];
+const B: string = datajson.Question[current_question_index].propositions[1];
+const C: string = datajson.Question[current_question_index].propositions[2];
+const D: string = datajson.Question[current_question_index].propositions[3];
+
+const h3_question = document.querySelector(".h3_question");
 if (h3_question) {
   h3_question.innerHTML = current_question;
 }
 
-const docA = document.querySelector('.A');
+const docA = document.querySelector(".A");
 if (docA) {
   docA.innerHTML = A;
 }
 
-const docB = document.querySelector('.B');
+const docB = document.querySelector(".B");
 if (docB) {
   docB.innerHTML = B;
 }
 
-const docC = document.querySelector('.C');
+const docC = document.querySelector(".C");
 if (docC) {
   docC.innerHTML = C;
 }
 
-const docD = document.querySelector('.D');
+const docD = document.querySelector(".D");
 if (docD) {
   docD.innerHTML = D;
+}
+
+// Ajouter un événement de clic sur le bouton_next
+if (button_next) {
+  button_next.addEventListener("click", () => {
+    // Vérifie si une réponse a été sélectionnée
+    const selected_response = document.querySelector(".selected");
+    if (!selected_response) {
+      alert("Veuillez sélectionner une réponse.");
+      return;
+    }
+    // Continuer l'exécution si une réponse a été sélectionnée
+    current_question_index++;
+
+    // Vérifie si toutes les questions ont été répondues
+    if (current_question_index === datajson.Question.length) {
+      // Cacher la section des questions et afficher la section de fin
+      if (section2 && section4) {
+        section2.classList.remove("show");
+        section2.classList.add("hide");
+        section4.classList.remove("hide");
+        section4.classList.add("show");
+      }
+    } else {
+      // Afficher la première question dès le début
+      const previous_question: string =
+        datajson.Question[current_question_index - 1].question;
+      const current_question: string =
+        datajson.Question[current_question_index].question;
+      const data_anecdote = datajson.Question[current_question_index - 1].anecdote;
+
+      const A = datajson.Question[current_question_index].propositions[0];
+      const B = datajson.Question[current_question_index].propositions[1];
+      const C = datajson.Question[current_question_index].propositions[2];
+      const D = datajson.Question[current_question_index].propositions[3];
+
+      const h3_question = document.querySelector(".h3_question");
+      if (h3_question) {
+        h3_question.innerHTML = current_question;
+      }
+
+      const h3_reponse = document.querySelector(".h3_reponse");
+      if (h3_reponse) {
+        h3_reponse.innerHTML = previous_question;
+      }
+
+      const docA = document.querySelector(".A");
+      if (docA) {
+        docA.innerHTML = A;
+      }
+
+      const docB = document.querySelector(".B");
+      if (docB) {
+        docB.innerHTML = B;
+      }
+
+      const docC = document.querySelector(".C");
+      if (docC) {
+        docC.innerHTML = C;
+      }
+
+      const docD = document.querySelector(".D");
+      if (docD) {
+        docD.innerHTML = D;
+      }
+
+      const anecdote = document.querySelector(".anecdote");
+      if (anecdote) {
+        anecdote.innerHTML = data_anecdote;
+      }
+
+      // Supprime la class selected
+      for (const reponse of reponses) {
+        if (reponse !== null) {
+          reponse.classList.remove("selected");
+        }
+      }
+    }
+  });
 }
 
 // Choix d'une questions
 
 for (const reponse of reponses) {
   reponse?.addEventListener("click", () => {
-    console.log("Réponse sélectionnée");
     // Retire la classe "selected" de toutes les réponses
     for (const autreReponse of reponses) {
       autreReponse?.classList.remove("selected");
@@ -140,126 +232,33 @@ for (const reponse of reponses) {
   });
 }
 
+// Boutton info / Anecdote
+if (button_info !== null && section2 !== null && section3 !== null) {
+  button_info.addEventListener("click", () => {
+    section2.classList.remove("show");
+    section2.classList.add("hide");
+    section3.classList.remove("hide");
+    section3.classList.add("show");
+  });
+}
 
+// Boutton next pour la section anecdote
 
+if (button_next_anecdote !== null && section2 !== null && section3 !== null) {
+  button_next_anecdote.addEventListener("click", () => {
+    section2.classList.remove("hide");
+    section2.classList.add("show");
+    section3.classList.remove("show");
+    section3.classList.add("hide");
+  });
+}
 
-
-
-
-
-
-
-// // Index de la question courante
-// let indexQuestionCourante = 0;
-
-// // Ajoute un écouteur d'événements "click" sur le bouton next
-// if (
-//   button_info !== null &&
-//   button_next !== null &&
-//   section1 !== null &&
-//   section2 !== null &&
-//   section3 !== null &&
-//   section4 !== null
-// ) {
-//   button_next.addEventListener("click", () => {
-//     // Récupère la réponse sélectionnée
-//     const reponseSelectionnee: HTMLElement | null = document.querySelector(
-//       ".reponses .selected"
-//     );
-
-//     // Vérifie si une réponse a été sélectionnée
-//     if (reponseSelectionnee !== null) {
-//       // Vérifie si la réponse sélectionnée est la bonne
-//       const reponseValide = reponsesValides[indexQuestionCourante];
-//       if (reponseSelectionnee.dataset.reponse === reponseValide) {
-//         // Incrémente le score dans le localstorage
-//         const score = localStorage.getItem("score");
-//         if (score !== null) {
-//           localStorage.setItem("score", (parseInt(score) + 1).toString());
-//         } else {
-//           localStorage.setItem("score", "1");
-//         }
-//       }
-
-//       // Sauvegarde la réponse sélectionnée dans le localstorage
-
-//       localStorage.setItem(
-//         `reponse_${indexQuestionCourante}`,
-//         reponseSelectionnee.dataset.reponse!
-//       );
-
-//       // Passe à la question suivante
-//       indexQuestionCourante++;
-//       const questionSuivante = document.querySelector(
-//         `#question_${indexQuestionCourante}`
-//       ) as HTMLElement;
-//       if (questionSuivante !== null) {
-//         section2.classList.remove("show");
-//         section2.classList.add("hide");
-//         section3.classList.remove("hide");
-//         section3.classList.add("show");
-//       } else {
-//         // Fin du quiz - Affiche le score final
-//         const scoreFinal = localStorage.getItem("score");
-//         if (scoreFinal !== null) {
-//           const scoreElement = document.getElementById("score_final");
-//           if (scoreElement !== null) {
-//             scoreElement.textContent = scoreFinal;
-//           }
-//         }
-//         section3.classList.remove("show");
-//         section3.classList.add("hide");
-//         section4.classList.remove("hide");
-//         section4.classList.add("show");
-//       }
-//     } else {
-//       alert("Veuillez sélectionner une réponse !");
-//     }
-//   });
-// }
-
-// fetch('../json/question_quizz.json')
-
-// interface Question {
-//   id: number;
-//   question: string;
-//   propositions: string[];
-//   reponse: string;
-//   anecdote: string;
-// }
-
-// const questions: Question[] = [
-//   // Insérez ici les questions du JSON
-// ];
-
-// const quizContainer: HTMLElement | null = document.querySelector(".questions");
-
-// function buildQuiz() {
-//   // Pour chaque question, générer le HTML pour la question et les réponses
-//   questions.forEach((question) => {
-//     const questionContainer = document.createElement("div");
-//     questionContainer.classList.add("question-container");
-
-//     const questionTitle = document.createElement("h2");
-//     questionTitle.innerText = question.question;
-//     questionContainer.appendChild(questionTitle);
-
-//     question.propositions.forEach((proposition) => {
-//       const label = document.createElement("label");
-//       const radio = document.createElement("input");
-//       radio.type = "radio";
-//       radio.name = `question-${question.id}`;
-//       radio.value = proposition;
-
-//       label.appendChild(radio);
-//       label.append(proposition);
-
-//       questionContainer.appendChild(label);
-//     });
-
-//     quizContainer?.appendChild(questionContainer);
-//   });
-
-//   // Ajouter un gestionnaire d'événements sur le bouton de soumission
-//   // button_next?.addEventListener("click", onSubmit);
-// }
+// Boutton Quitter
+if (button_fin !== null && section1 !== null && section4 !== null) {
+  button_fin.addEventListener("click", () => {
+    section1.classList.remove("hide");
+    section1.classList.add("show");
+    section4.classList.remove("show");
+    section4.classList.add("hide");
+  });
+}
